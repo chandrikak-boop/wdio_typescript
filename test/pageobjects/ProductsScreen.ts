2e93a1630eec6ecddb5d7c95220b3a82b5d480c5
chandrikak-boop <chandrikak@testvagrant.com>
2025-12-29 16:55:11 +0530

    locator issue resolved

--- test/pageobjects/ProductsScreen.ts
+++ test/pageobjects/ProductsScreen.ts
@@ -1,30 +1,84 @@
 class ProductsScreen {
 
-  get addToCartBtn() {
-  if (driver.isAndroid) {
-    return $('//*[@content-desc="test-ADD TO CART"]')
+  get addToCartButtons() {
+    if (driver.isAndroid) {
+      return $$('//*[@content-desc[contains(., "test-ADD TO CART")]]')
+    }
+    return $$('~test-ADD TO CART')
   }
-  return $('~ADD TO CART'); 
-  // or $('-ios predicate string:name == "ADD TO CART"');
-}
 
-get cartIcon() {
-  if (driver.isAndroid) {
-    return $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView');
+  get cartIcon() {
+    return $('~test-Cart')
   }
-  return $('~test-Cart');
-}
-
 
   async addItemToCart() {
-    await this.addToCartBtn.waitForDisplayed({timeout:10000})
-    await this.addToCartBtn.click()
-    await expect($('~test-REMOVE')).toBeDisplayed();
+
+    // CI-safe scroll
+    await driver.execute('mobile: scroll', { direction: 'down' })
+
+    const buttons = this.addToCartButtons
+
+    await browser.waitUntil(
+      async () => (await buttons.length) > 0,
+      { timeout: 15000, timeoutMsg: 'No ADD TO CART buttons found' }
+    )
+
+    const firstBtn = buttons[0]
+    await firstBtn.waitForDisplayed({ timeout: 10000 })
+    await firstBtn.click()
+
+    // Validate REMOVE appeared for first product
+    if (driver.isAndroid) {
+      await expect($('//*[@content-desc[contains(., "test-REMOVE")]]')).toBeDisplayed()
+    } else {
+      await expect($('~test-REMOVE')).toBeDisplayed()
+    }
   }
 
   async openCart() {
+    await this.cartIcon.waitForDisplayed({ timeout: 10000 })
     await this.cartIcon.click()
   }
 }
 
 export default new ProductsScreen()
+
+
+
+
+
+
+
+
+//-------------------------------------------------
+
+// class ProductsScreen {
+
+//   get addToCartBtn() {
+//   if (driver.isAndroid) {
+//     return $('//*[@content-desc="test-ADD TO CART"]')
+//   }
+//   return $('~ADD TO CART'); 
+//   // or $('-ios predicate string:name == "ADD TO CART"');
+// }
+
+// get cartIcon() {
+//   if (driver.isAndroid) {
+//     return $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView');
+//   }
+//   return $('~test-Cart');
+// }
+
+
+//   async addItemToCart() {
+//     await this.addToCartBtn.waitForDisplayed({timeout:10000})
+//     await this.addToCartBtn.click()
+//     await expect($('~test-REMOVE')).toBeDisplayed();
+//   }
+
+//   async openCart() {
+//     await this.cartIcon.click()
+//   }
+// }
+
+// export default new ProductsScreen()
