e2a5c4d93d308922db03e304f69ae80b7bf2a15e
chandrikak-boop <chandrikak@testvagrant.com>
2025-12-29 17:39:36 +0530

    locator issue resolved

--- test/pageobjects/ProductsScreen.ts
+++ test/pageobjects/ProductsScreen.ts
@@ -1,32 +1,68 @@
 class ProductsScreen {
 
   get addToCartBtn() {
-  if (driver.isAndroid) {
-    return $('android=new UiSelector().resourceId("com.swaglabsmobileapp:id/addToCart")');
-
-
+    if (driver.isAndroid) {
+      return $$('~test-ADD TO CART')[0];
+    }
+    return $$('~ADD TO CART')[0];
   }
-  return $('~ADD TO CART'); 
-  // or $('-ios predicate string:name == "ADD TO CART"');
-}
 
-get cartIcon() {
-  if (driver.isAndroid) {
-    return $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView');
+  get cartIcon() {
+    if (driver.isAndroid) {
+      return $('~test-Cart');
+    }
+    return $('~test-Cart');
   }
-  return $('~test-Cart');
-}
-
 
   async addItemToCart() {
-    await this.addToCartBtn.waitForDisplayed({timeout:10000})
-    await this.addToCartBtn.click()
+    await this.addToCartBtn.waitForDisplayed({ timeout: 15000 });
+    await this.addToCartBtn.click();
     await expect($('~test-REMOVE')).toBeDisplayed();
   }
 
   async openCart() {
-    await this.cartIcon.click()
+    await this.cartIcon.waitForDisplayed();
+    await this.cartIcon.click();
   }
 }
 
-export default new ProductsScreen()
+export default new ProductsScreen();
+
+
+
+
+
+
+
+// class ProductsScreen {
+
+//   get addToCartBtn() {
+//   if (driver.isAndroid) {
+//     return $('android=new UiSelector().resourceId("com.swaglabsmobileapp:id/addToCart")');
+
+
+//   }
+//   return $('~ADD TO CART'); 
+//   // or $('-ios predicate string:name == "ADD TO CART"');
+// }
+
+// get cartIcon() {
+//   if (driver.isAndroid) {
+//     return $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView');
+//   }
+//   return $('~test-Cart');
+// }
+
+
+//   async addItemToCart() {
+//     await this.addToCartBtn.waitForDisplayed({timeout:10000})
+//     await this.addToCartBtn.click()
+//     await expect($('~test-REMOVE')).toBeDisplayed();
+//   }
+
+//   async openCart() {
+//     await this.cartIcon.click()
+//   }
+// }
+
+// export default new ProductsScreen()
