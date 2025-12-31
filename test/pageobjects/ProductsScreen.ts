8fd6a38844c48475de67aab9a1b05d5a00f705d7
chandrikak-boop <chandrikak@testvagrant.com>
2025-12-31 18:16:15 +0530

    xpath locator used

--- test/pageobjects/ProductsScreen.ts
+++ test/pageobjects/ProductsScreen.ts
@@ -1,61 +1,8 @@
-/*class ProductsScreen {
-
-  // ====== LOCATORS ======
-
-  get firstAddToCartBtn() {
-    if (driver.isAndroid) {
-      return $(
-        '//android.view.ViewGroup[contains(@content-desc,"ADD TO CART")]'
-      );
-    }
-
-    // iOS
-    return $(
-      '//XCUIElementTypeButton[contains(@name,"ADD TO CART")]'
-    );
-  }
-
-  get removeBtn() {
-    if (driver.isAndroid) {
-      return $(
-        '//android.view.ViewGroup[contains(@content-desc,"REMOVE")]'
-      );
-    }
-
-    return $(
-      '//XCUIElementTypeButton[contains(@name,"REMOVE")]'
-    );
-  }
-
-  get cartIcon() {
-    return $('~test-Cart');
-  }
-
-  // ====== ACTIONS ======
-
-  async addFirstProductToCart() {
-    // Wait for products list via Add to Cart visibility
-    await this.firstAddToCartBtn.waitForDisplayed({ timeout: 20000 });
-    await this.firstAddToCartBtn.click();
-
-    // Validate item added
-    await expect(this.removeBtn).toBeDisplayed();
-  }
-
-  async openCart() {
-    await this.cartIcon.waitForDisplayed({ timeout: 10000 });
-    await this.cartIcon.click();
-  }
-}
-
-export default new ProductsScreen();
-
-*/
 class ProductsScreen {
 
   get addToCartBtn() {
   if (driver.isAndroid) {
-return $('~test-ADD TO CART');
+return $('(//android.view.ViewGroup[@content-desc="test-ADD TO CART"])[1]');
 
 
   }
